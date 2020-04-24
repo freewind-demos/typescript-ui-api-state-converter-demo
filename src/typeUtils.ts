b5e888652815500d3b1c468a42dddecbd27ddd22
Freewind <nowindlee@gmail.com>
2020-04-24 20:36:58 +0800

    Fixed "PartOf" which will not allow extra properties

--- src/typeUtils.ts
+++ src/typeUtils.ts
@@ -1,1 +1,10 @@
-export type PartOf<T, X extends Partial<T>> = X
+export type DeepPartial<T> = { [K in keyof T]?: DeepPartial<T[K]> };
+
+export type DeepNoExcess<T, U> = {
+  [K in keyof U]:
+  K extends keyof T ? DeepNoExcess<Required<T>[K], U[K]> :
+    never
+};
+
+// https://stackoverflow.com/questions/61393505/how-to-define-a-type-and-make-sure-its-part-of-another-type-in-typescript
+export type PartOf<T, U extends DeepPartial<T> & DeepNoExcess<T, U>> = U;
